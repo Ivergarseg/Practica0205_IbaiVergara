2935c5e5e775ea650d3da65d940c6cf6beb2cf6d
Ibai <ivergarseg2@educacion.navarra.es>
2023-11-13 16:47:54 +0100

    Ahora solo te acepta los numeros que corresponden en cada input

--- Ejercicio 6.py
+++ Ejercicio 6.py
@@ -6,10 +6,12 @@ def binario_to_decimal(numbinario):
     lista = list(numbinario)
     lista_a_sumar = []
     for valor in range(len(lista)):
-        if lista[valor] == '1':
+        if lista[valor] != '0' and lista[valor]!= '1':
+            return 'El número no es un número binario.'
+        elif lista[valor] == '1':
             elem_a_sumar = 2**(len(lista) - valor - 1)
             lista_a_sumar.append(elem_a_sumar)
-    
+        
     valor_decimal = sum(lista_a_sumar)
     return valor_decimal
 
@@ -22,10 +24,10 @@ def decimal_to_binario(numdecimal):
     exp = 0
     resto = 0
     lista_solucion = []
-    while 2**exp <= numdecimal:
-        exp += 1
+    while 2**exp <= numdecimal: #busco el exponente con el 
+        exp += 1                #que voy a tener que empezar a trabajar
      
-    for i in reversed(range(exp)):
+    for i in reversed(range(exp)): #trabajo con los exp en orden descendente
         if numdecimal - 2**(i) >= 0:
             resto = numdecimal - 2**(i)
             lista_solucion.append('1')
@@ -36,10 +38,6 @@ def decimal_to_binario(numdecimal):
     solucion = ''.join(lista_solucion)
     
     return solucion
-
-
-
-
 
 
 numdecimal = int(input('Ingrese un número decimal: '))
