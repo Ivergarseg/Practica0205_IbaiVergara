7879765e8033f33eeb9a7ce594992553c5b2cd13
Ibai <ivergarseg2@educacion.navarra.es>
2023-11-13 16:02:02 +0100

    Ejercicio 7 hecho

---
+++ Ejercicio 6.py
@@ -0,0 +1,46 @@
+'''Escribir una función que convierta un número decimal en binario 
+y otra que convierta un número binario en decimal.'''
+
+def binario_to_decimal(numbinario):
+    '''Dado un número binario, devuelva su valor decimal.'''
+    lista = list(numbinario)
+    lista_a_sumar = []
+    for valor in range(len(lista)):
+        if lista[valor] == '1':
+            elem_a_sumar = 2**(len(lista) - valor - 1)
+            lista_a_sumar.append(elem_a_sumar)
+    
+    valor_decimal = sum(lista_a_sumar)
+    return valor_decimal
+
+numbinario = input('Ingrese un número binario sin espacios: ')
+
+print(binario_to_decimal(numbinario))
+
+def decimal_to_binario(numdecimal):
+    '''Dado un número decimal, devuelva su valor binario.'''
+    exp = 0
+    resto = 0
+    lista_solucion = []
+    while 2**exp <= numdecimal:
+        exp += 1
+     
+    for i in reversed(range(exp)):
+        if numdecimal - 2**(i) >= 0:
+            resto = numdecimal - 2**(i)
+            lista_solucion.append('1')
+            numdecimal = resto
+        else: 
+            lista_solucion.append('0')
+
+    solucion = ''.join(lista_solucion)
+    
+    return solucion
+
+
+
+
+
+
+numdecimal = int(input('Ingrese un número decimal: '))
+print (decimal_to_binario(numdecimal))
